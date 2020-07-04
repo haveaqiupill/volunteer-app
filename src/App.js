b71daa4094fe00d71fb1c2432fbfb057d9a5dcc4
Jing Ying <e0323887@u.nus.edu>
2020-07-04 10:26:37 +0800

    Add onepirate theme and install relevant dependencies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import withRoot from "./onepirate/modules/withRoot";
+
+import NavbarView from "./onepirate/modules/views/NavbarView";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Fragment>
+          <NavbarView />
+      </Fragment>
   );
 }
 
-export default App;
+export default withRoot(App);
