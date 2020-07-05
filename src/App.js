0fbbce0bbd3d810cd0450f347655cd1ed8ab5daf
Jing Ying <e0323887@u.nus.edu>
2020-07-05 13:39:04 +0800

    Replace template details in Home Page with Application information

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import NavbarView from "./onepirate/modules/views/NavbarView";
 import ProductHero from "./onepirate/modules/views/ProductHero";
 import ProductValues from "./onepirate/modules/views/ProductValues";
 import ProductCategories from "./onepirate/modules/views/ProductCategories";
-import ProductHowItWorks from "./onepirate/modules/views/ProductHowItWorks";
-import ProductCTA from "./onepirate/modules/views/ProductCTA";
 import ProductSmokingHero from "./onepirate/modules/views/ProductSmokingHero";
 import AppFooter from "./onepirate/modules/views/AppFooter";
 
@@ -14,12 +12,10 @@ import AppFooter from "./onepirate/modules/views/AppFooter";
 function App() {
   return (
       <Fragment>
-          <NavbarView title=""/>
+          <NavbarView title="RSearchV"/>
           <ProductHero />
           <ProductValues />
           <ProductCategories />
-          <ProductHowItWorks />
-          <ProductCTA />
           <ProductSmokingHero />
           <AppFooter />
       </Fragment>
