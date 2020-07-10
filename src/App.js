b956a7af3207f38b56f81856820a489f140e45a1
tlim045 <tlim045@e.ntu.edu.sg>
2020-07-10 13:54:03 +0800

    add extended sign up page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import ListView from "./pages/ListView";
+import SignUpResearcher from "./pages/SignUpResearcher";
 
 import "./util/firebase.js";
 
@@ -16,7 +17,7 @@ function App() {
 
       <SignUp path="/sign-up" />
       <SignIn path="/sign-in" />
-
+      <SignUpResearcher path="/sign-up-researcher" />
       <ListView path="/programs" />
     </Router>
   );
--- src/pages/SignUpVolunteer.js
+++ src/pages/SignUpVolunteer.js
@@ -9,13 +9,10 @@ import Typography from "../modules/components/Typography";
 import AppFooter from "../modules/views/AppFooter";
 import AppAppBar from "../modules/views/AppAppBar";
 import AppForm from "../modules/views/AppForm";
-import { email, required } from "../modules/form/validation";
+import {required } from "../modules/form/validation";
 import RFTextField from "../modules/form/RFTextField";
 import FormButton from "../modules/form/FormButton";
 import FormFeedback from "../modules/form/FormFeedback";
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Radio from '@material-ui/core/Radio';
-import Authentication from "../util/Authentication";
 
 const useStyles = makeStyles((theme) => ({
   form: {
@@ -30,43 +27,21 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function SignUp() {
+function SignUpResearcher() {
   const classes = useStyles();
   const [sent, setSent] = React.useState(false);
 
   const validate = (values) => {
     const errors = required(
-      ["firstName", "lastName", "email", "password"],
+      ["organization", "researchArea", "shortIntroduction"],
       values
-    );
-
-    if (!errors.email) {
-      const emailError = email(values.email, values);
-      if (emailError) {
-        errors.email = email(values.email, values);
-      }
-    }
+    )
 
     return errors;
   };
 
-  const [selectedValue, setSelectedValue] = React.useState('researcher');
-  
-  const handleChange = (event) => {
-      setSelectedValue(event.target.value);
-      console.log(event.target.value);
-    }
-
   const handleSubmit = (values) => {
-    // TODO: get radio buttons value as userType
-    const { email, password, firstName, lastName, userType } = values;
-    
-    try {
-      Authentication.signUp(email, password, firstName, lastName, userType);
-    } catch (error) {
-    // TODO: show the error message to user
-    }
-    // TODO: Redirect base on userType
+    //TODO
     setSent(true);
   };
 
@@ -92,77 +67,36 @@ function SignUp() {
           {({ handleSubmit, submitting }) => (
             <form onSubmit={handleSubmit} className={classes.form} noValidate>
               <Grid container spacing={2}>
-                <Grid item xs={12} sm={7}>
-                  <Typography variant="h5">I am a...</Typography>
-                </Grid>
-                <Grid item xs={3} sm={12}>
-                <FormControlLabel
-                  value="researcher"
-                  control = {
-                  <Radio
-                  checked={selectedValue === 'researcher'}
-                  onChange={handleChange}
-                  inputProps={{ 'aria-label': 'A' }}
-                />
-                }
-                  label="Researcher"
-                />
-                <FormControlLabel
-                  value="volunteer"
-                  control = {
-                  <Radio
-                  checked={selectedValue === 'volunteer'}
-                  onChange={handleChange}
-                  inputProps={{ 'aria-label': 'B' }}
-                />
-                  }
-                  label="Volunteer"
-                />
+                <Grid item xs={12} sm={6}>
+                  <Field
+                    autoFocus
+                    component={RFTextField}
+                    halfWidth
+                    label="Organization"
+                    name="organization"
+                    required
+                  />
                 </Grid>
                 <Grid item xs={12} sm={6}>
                   <Field
                     autoFocus
                     component={RFTextField}
-                    autoComplete="fname"
                     fullWidth
-                    label="First name"
-                    name="firstName"
+                    label="Faculty"
+                    name="Faculty"
                     required
                   />
                 </Grid>
-                <Grid item xs={12} sm={6}>
+                <Grid item xs={12} sm={12}>
                   <Field
                     component={RFTextField}
-                    autoComplete="lname"
                     fullWidth
-                    label="Last name"
-                    name="lastName"
+                    label="Short Introduction"
+                    name="shortIntroduction"
                     required
                   />
                 </Grid>
               </Grid>
-
-              <Field
-                autoComplete="email"
-                component={RFTextField}
-                disabled={submitting || sent}
-                fullWidth
-                label="Email"
-                margin="normal"
-                name="email"
-                required
-              />
-              <Field
-                fullWidth
-                component={RFTextField}
-                disabled={submitting || sent}
-                required
-                name="password"
-                autoComplete="current-password"
-                label="Password"
-                type="password"
-                margin="normal"
-              />
               <FormSpy subscription={{ submitError: true }}>
                 {({ submitError }) =>
                   submitError ? (
@@ -178,7 +112,7 @@ function SignUp() {
                 color="secondary"
                 fullWidth
               >
-                {submitting || sent ? "In progress…" : "Next"}
+                {submitting || sent ? "In progress…" : "Sign Up"}
               </FormButton>
             </form>
           )}
@@ -187,6 +121,6 @@ function SignUp() {
       <AppFooter />
     </React.Fragment>
   );
-}
 
-export default withRoot(SignUp);
+}
+export default withRoot(SignUpResearcher);
