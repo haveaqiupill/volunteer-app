cb1836a8d2ac461758ab01151f6d882d6ffb18d6
Jing Ying <e0323887@u.nus.edu>
2020-07-13 18:17:41 +0800

    Change organization field to a selectable type

--- src/pages/ProgramsList/ListItem.js
+++ src/pages/ProgramsList/ListItem.js
@@ -50,6 +50,7 @@ const ListItem = ({ item, showModal }) => {
         <img
           height={200}
           alt="logo"
+          //TODO: Render image based on organization of researcher who posted the program
           src={require(item.image === 0
             ? "../../images/nus_logo.png"
             : item.image === 1
